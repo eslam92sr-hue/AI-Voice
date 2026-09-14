c3e7a8890b1a3b9331ddf8c91d43cfbf3ca74e67
eslam92sr-hue <eslam92sr@gmail.com>
2026-09-14 14:48:33 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,10 @@
+"use strict";
+ */
+
+const SUPABASE_URL = https://ufvcmgoihwmwzfowjfez.supabase.co
+const SUPABASE_KEY = sb_publishable_rIigl5nN-kIyg3Pbe2HAUg_0_c7MoTu
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
